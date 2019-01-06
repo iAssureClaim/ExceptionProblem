0669abe7858e4210544f04119006769a3ac8ddf4
iAssureClaim <46375129+iAssureClaim@users.noreply.github.com>
2019-01-06 13:42:14 +0530

    Add files via upload

---
+++ src/com/cg/insurance/client/InsuranceMain.java
@@ -0,0 +1,144 @@
+package com.cg.insurance.client;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+import com.cg.insurance.bean.InsuranceClaimBean;
+import com.cg.insurance.bean.UserBean;
+import com.cg.insurance.exception.InsuranceClaimException;
+import com.cg.insurance.service.IInsuranceService;
+import com.cg.insurance.service.InsuranceServiceImpl;
+
+public class InsuranceMain {
+	static Scanner sc=new Scanner(System.in);
+	static UserBean userbean=null;
+	static InsuranceClaimBean claimBean=null;
+	static IInsuranceService iInsuranceService=null;
+	static InsuranceServiceImpl insuranceServiceImpl=null;
+	
+public static void main(String[] args) throws IOException, SQLException, InsuranceClaimException {
+	
+	userbean = new UserBean();
+	String rolecode=null;
+	System.out.println("Enter Your Credentials to use your facility: ");
+	System.out.println("Enter your Username: ");
+	userbean.setUsername(sc.next());
+	System.out.println("Enter your Password: ");
+	userbean.setPassword(sc.next());
+	
+	
+	iInsuranceService = new InsuranceServiceImpl();
+	rolecode=iInsuranceService.checkAccess(userbean);
+	int option;
+	switch (rolecode.toLowerCase()) {
+	case "insured":
+		
+		while(true)
+		{
+				System.out.println("\n----------------Welcome User--------------------\n");
+				System.out.println("1.Create Claim as per Your Policies.");
+				System.out.println("2.View Claim.");
+				System.out.println("3.Exit.\n");
+				
+				try {
+					option = sc.nextInt();
+					switch (option) {
+					case 1:
+						
+						break;
+					case 2:
+						
+						break;
+					case 3:
+						System.exit(0);
+						break;
+					default:
+						System.out.println("Please Select only from above options");
+						break;
+					}
+				}                         
+				catch (InputMismatchException e) {
+						sc.nextLine();
+						System.out.println("Please enter only Numeric Value.");
+				}   
+		}	
+		
+	case "agent":
+		while(true)
+		{
+			System.out.println("\n-------------------Welcome Agent----------------\n");
+			System.out.println("1.Create Claim for Your Customers.");
+			System.out.println("2.View Claim of your Customers.");
+			System.out.println("3.Exit.\n");
+			try {
+				option = sc.nextInt();
+				switch (option) {
+				case 1:
+					
+					break;
+				case 2:
+					
+					break;
+				case 3:
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Please Select only from above options");
+					break;
+				}
+			}                         
+			catch (InputMismatchException e) {
+					sc.nextLine();
+					System.out.println("Please enter only Numeric Value.");
+			}   
+	    }	
+		
+	case "admin":
+		while(true)
+		{
+			System.out.println("\n-------------------Welcome Admin----------------\n");
+			System.out.println("1.Create Profile.");
+			System.out.println("2.Create Claim for any customer.");
+			System.out.println("3.View Claim of any customer.");
+			System.out.println("4.Generate Report.");
+			System.out.println("5.Exit.\n");
+			try {
+				option = sc.nextInt();
+				switch (option) {
+				case 1:
+					
+					break;
+				case 2:
+					
+					break;
+				case 3:
+					
+					break;
+				case 4:
+	
+					break;
+				case 5:
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Please Select only from above options");
+					break;
+				}
+			}                         
+			catch (InputMismatchException e) {
+					sc.nextLine();
+					System.out.println("Please enter only Numeric Value.");
+			}   
+	}	
+	default:
+		System.out.println("Please Select only from above options");
+		break;
+	}
+	
+
+	
+}
+}
---
+++ src/com/cg/insurance/exception/InsuranceClaimException.java
@@ -0,0 +1,30 @@
+package com.cg.insurance.exception;
+
+public class InsuranceClaimException extends Exception {
+
+	public InsuranceClaimException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InsuranceClaimException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public InsuranceClaimException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InsuranceClaimException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public InsuranceClaimException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+}
